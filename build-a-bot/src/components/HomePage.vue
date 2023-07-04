c9fa53298b583131be5ce8a34962da6b8103cd39
Karolina Dbaj <k.dbaj@outlook.com>
2023-07-04 12:41:30 +0200

    created the home page

---
+++ build-a-bot/src/components/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true"/>
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+
+.home {
+  text-align: center;
+  font-family: 'Roboto', sans-serif;
+  font-size: 24px;
+}
+
+.robot {
+  height: 300px;
+}
+</style>
