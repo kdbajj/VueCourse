70e33bc1aa2bec5d2f02d2fcdc93d3bd5f508fce
Karolina Dbaj <k.dbaj@outlook.com>
2023-07-31 12:31:38 +0200

    conditionally displaying content with v-if and v-show

--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -4,6 +4,7 @@
       <div class="top part">
         <div class="robot-name">
         {{ selectedRobot.head.title }}
+        <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
       </div>
         <img v-bind:src="selectedRobot.head.src" title="head" />
         <button @click="selectPreviousHead()" class="prev-selector">&#9668;</button>
@@ -233,5 +234,8 @@ export default {
   top: -25px;
   text-align: center;
   width: 100%;
+}
+.sale{
+  color: red;
 }
 </style>
